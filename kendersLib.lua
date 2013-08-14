346d2ff7816f7695dd9bf98fc8fc11b01f501182
Pawel 'Kender' Maczewski <kender@codingslut.com>
2013-08-15 00:58:02 +0200

    GGData submodule added
    additional params for Device model detection

--- kendersLib.lua
+++ kendersLib.lua
@@ -1,4 +1,7 @@
-module( ..., package.seeall )
+local pathOfThisFile = ...
+local folderOfThisFile = (...):match("(.-)[^%.]+$")
+
+module( pathOfThisFile, package.seeall )
 
 local KLCache = {}
 
@@ -340,13 +343,16 @@ M.isAndroid = false
 M.isGoogle = false
 M.isKindleFire = false
 M.isNook = false
+M.isConsole = false
+M.isSamsung = false
 M.is_iPad = false
 M.isTall = false
 M.isSimulator = false
+M.platform = nil
 
 local model = system.getInfo("model")
+
 M.model = model
-print (">>> MODEL: >>> ", model)
 -- Are we on the Simulator?
 if ( "simulator" == system.getInfo("environment") ) then
     M.isSimulator = true
@@ -364,7 +370,6 @@ if ( string.sub( model, 1, 2 ) == "iP" ) then
         M.is_iPad = true
     end
 else
-    print ("Not Apple")
     -- Not Apple, so it must be one of the Android devices
     M.isAndroid = true
  
@@ -383,7 +388,25 @@ else
         M.isNook = true
         M.isGoogle = false --revert Google Play to false
     end
- 
+    
+    if ( string.sub ( model, 1, 9 ) == "GameStick") or ( string.sub ( model, 1, 4 ) == "OUYA" ) then
+        M.isGoogle = false
+        M.isConsole = true
+    end
+end
+print ( "Model: " )
+for _, v in pairs ( M ) do
+    if ( v ) then
+        print (_, " => ", v)
+    end
 end
 
 _G["_MODEL"] = M
+
+
+-- persistent data storage, using GGData module ( https://github.com/GlitchGames/GGData.git )
+local GGData = require ( folderOfThisFile .. "GG.GGData.corona.GGData" )
+local savedState = GGData:new( "savedState" )
+savedState:save()
+
+_G["savedState"] = savedState
