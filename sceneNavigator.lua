69955d68d00389307c4ecf2b412b442ffb939fac
Pawel 'Kender' Maczewski <kender@codingslut.com>
2013-08-20 15:24:45 +0200

    specified scenes can be set as hiding the nag bar image can be used as nav bar popScene improvements

--- sceneNavigator.lua
+++ sceneNavigator.lua
@@ -5,6 +5,7 @@
 -- ************************************************************************************************************************
 
 -- to use the navigation bar you should 
+--   set navigator.isNavigationBarVisible to true,
 -- add your displayObjects to a new group, instead of scene.view
 -- and set that group as scene.group
 -- 
@@ -23,31 +24,63 @@ local navigator = storyboard.navigator
 navigator.navigationStack = {}
 navigator.currentScene = nil
 navigator.isNavigationBarVisible = false
+navigator.navigationBarColor = nil
+navigator.navigationBarImage = nil
+navigator.navigationBarHiddenScenes = {}
 
 function navigator:pushScene ( sceneName, params )
     local currentScene = storyboard.getCurrentSceneName()
-    print ( "Current scene: ", currentScene )
+    print ( "Pushing. ", currentScene, " --> ", sceneName )
     -- if ( ( #self.navigationStack == 0 and currentScene ~= nil ) or self.navigationStack [ #self.navigationStack ] ~= currentScene ) then
     --     self.navigationStack [ #self.navigationStack + 1 ] = currentScene
     -- end
-    self.navigationStack [ #self.navigationStack + 1 ] = currentScene
     if params == nil then
         params = {}
     end
     
     local newScene = storyboard.loadScene ( sceneName, false, params.params )
+    self.navigationStack [ #self.navigationStack + 1 ] = { newScene, newScene.title, sceneName, params }
     if newScene.group then
         newScene.view:insert ( newScene.group )        
     end
-    if navigator.isNavigationBarVisible then
+    
+    local isBarVisible = navigator.isNavigationBarVisible
+    
+    if table.contains ( navigator.navigationBarHiddenScenes, sceneName ) then
+        isBarVisible = false
+    end
+    
+    if isBarVisible then
         if newScene.group then
-            newScene.group.y = newScene.group.y + 33
             local rectG = display.newGroup ()
-            local rect = display.newRect( rectG, 0, 0, display.contentWidth, 33 )
             newScene.view:insert ( rectG )
-            rectG:insert ( rect )
-            if #self.navigationStack >= 1 then
-                local title = self.navigationStack [ #self.navigationStack ]
+            if self.navigationBarImage == nil then
+                local rect = display.newRect( rectG, display.screenLeft, 0, display.contentWidth, 33 )
+                local color = { 255, 255, 255, 255 }
+                if navigator.navigationBarColor ~= nil then
+                    print ( "Color is defined" )
+                    color = navigator.navigationBarColor
+                    if color [ 4 ] == nil then
+                        color [ 4 ] = 255
+                    end
+                end
+                print ( color [ 4 ])
+                rect:setFillColor ( color [ 1 ], color [ 2 ], color [ 3 ], color [ 4 ])
+                rectG:insert ( rect )
+            else
+                local img = display.newImageRect ( rectG, self.navigationBarImage, display.contentWidth, 33 )
+                img.x, img.y = display.screenLeft, display.screenTop
+                rectG:insert ( img )
+            end
+            newScene.group.y = newScene.group.y + 33
+            if #self.navigationStack > 1 then
+                local title = self.navigationStack [ #self.navigationStack - 1 ]
+                if title [ 2 ] == nil then
+                    title = "Back"
+                else
+                    title = title [ 2 ]
+                end
+                
                 local backB = widget.newButton ({
                     label = "<- " .. title,
                     width = 80,
@@ -57,7 +90,7 @@ function navigator:pushScene ( sceneName, params )
                         self:popScene ()
                     end,
                 })
-                backB.x = 100
+                backB.x = display.screenLeft + 40 + 2
                 backB.y = 16
                 rectG:insert ( backB )
             end
@@ -68,11 +101,37 @@ end
 
 function navigator:popScene ()
     
-    if ( #self.navigationStack > 0 ) then
+    if ( #self.navigationStack > 1 ) then
         local currentScene = storyboard.getCurrentSceneName()
-        local newScene = table.popLast ( self.navigationStack )
-        storyboard.gotoScene ( newScene )
+        local cs = table.popLast ( self.navigationStack )
+        local newScene = self.navigationStack [ #self.navigationStack ]
+        print ( "Popping. ", currentScene, " --> ", newScene [ 3 ] )
+        local params = newScene [ 4 ]
+        if type ( params ) == "string" then
+            params = { effect = params }
+        end
+        print ( "Params: ", params )
+        if cs [ 4 ] and cs [ 4 ].effect then
+            local effect = cs [ 4 ].effect            
+            if effect == "slideDown" then
+                params.effect = "slideUp"
+            end
+            if effect == "slideLeft" then
+                params.effect = "slideRight"
+            end
+            if effect == "crossFade" then
+                params.effect = effect
+            end
+        end
+        storyboard.gotoScene ( newScene [3], params )
         storyboard.purgeScene ( currentScene )
     end
 end
 
+storyboard.pushScene = function ( sceneName, params )
+    storyboard.navigator:pushScene ( sceneName, params )
+end
+
+storyboard.popScene = function () 
+    storyboard.navigator:popScene ()
+end
